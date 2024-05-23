9aa683494f92577771ac6cade85830f806fb79bf
Swapnita Singh <swapimamuni@gmail.com>
2024-05-24 02:23:24 +0530

    updatechanges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [email,setEmail]= useState("");
   const [password,setPassword]= useState("");
   const [data, setData] = useState([]); 
+  const [editingUserId, setEditingUserId] = useState(null);
   
   const addData = async(e)=>{
     e.preventDefault();
@@ -49,6 +50,33 @@ function App() {
     fetchData();
   }
 
+  const editUser = (user) => {
+    setEditingUserId(user._id);
+    setName(user.name);
+    setRoll(user.roll);
+    setEmail(user.email);
+    setPassword(user.password);
+   
+  }
+
+  const updateData = async () => {
+    let result = await fetch(`http://localhost:4500/update/${editingUserId}`, {
+      method: "PUT",
+      body: JSON.stringify({ name, roll, email, password }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    setEditingUserId(null); // Clear editing user state
+    fetchData();
+
+    setName("");
+  setRoll("");
+  setEmail("");
+  setPassword("");
+  }
  
 
   return (
@@ -58,9 +86,12 @@ function App() {
      <input className="inputbox" type='text' value={roll} onChange={(e)=>setRoll(e.target.value)}  placeholder='Enter roll number'/>
      <input className="inputbox" type='text' value= {email} onChange={(e)=>setEmail(e.target.value)}  placeholder='Enter email'/>
      <input className="inputbox" type='password' value= {password} onChange={(e)=>setPassword(e.target.value)}   placeholder='Enter password'/>
-       
+
+     {editingUserId ? (
+        <button onClick={updateData} className='button' type="button">Update</button>
+      ) : (  
      <button onClick={addData}  className='button' type="button">Submit</button>
-     
+    )}
      <h1 style={{ textAlign: 'center' }}>Display Data</h1>
       <ul>
         {data.map(users => (
@@ -68,7 +99,7 @@ function App() {
             Name: {users.name}, Roll Number: {users.roll}, Email: {users.email}
             
             <button onClick={() => deleteData(users._id)} className='button' type="button">Delete</button>
-            <button  className='button' type="button">Edit</button>
+            <button onClick={() => editUser(users)} className='button' type="button">Edit</button>
           </li>
         ))}
       </ul>
